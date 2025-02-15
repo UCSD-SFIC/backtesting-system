9af29a6de8df550f1720713b5782cf2aedc4e9ca
hlm01 <35647316+hlm01@users.noreply.github.com>
2025-02-15 12:00:01 -0800

    fix return calculations

--- backtest.py
+++ backtest.py
@@ -7,7 +7,7 @@ import polars as pl
 import os
 
 
-def load_data(tickers):
+def load_data(tickers, timespan, from_time, to_time):
     """
     Loads stock data from Polygon.io API for the given tickers.
     If the data is not cached, it will be downloaded and saved as a parquet file.
@@ -16,7 +16,9 @@ def load_data(tickers):
     histories = {}
     cache_dir = "history_cache"
     for ticker in tickers:
-        path = os.path.join(cache_dir, f"{ticker}.parquet")
+        path = os.path.join(
+            cache_dir, f"{ticker}-{timespan}-{from_time}-{to_time}.parquet"
+        )
         try:
             df = pl.read_parquet(path)
             histories[ticker] = df
@@ -25,9 +27,9 @@ def load_data(tickers):
             aggs = client.get_aggs(
                 ticker=f"{ticker}",
                 multiplier=1,
-                timespan="year",
-                from_="2000-01-01",
-                to="2025-02-09",
+                timespan=timespan,
+                from_=from_time,
+                to=to_time,
             )
             df = pl.DataFrame(aggs)
             if not os.path.exists(cache_dir):
@@ -37,62 +39,117 @@ def load_data(tickers):
     return histories
 
 
-def backtest(history, weights):
+def backtest(history, weights, tickers):
     """
     Backtests the given weights on the given stock data.
     Returns a DataFrame with the cumulative return and daily return.
     """
     backtest = weights.lazy()
-    ticker_return_expressions = []  # daily return calculation for each stock
 
-    # combine stock data into one DataFrame
+    combined_history = None
     for ticker in tickers:
-        backtest = backtest.join(
+        df = (
             history[ticker]
             .lazy()
-            .select(["timestamp", pl.col("close").alias(f"{ticker}_close")]),
-            on="timestamp",
-            how="inner",
+            .select(
+                [
+                    pl.from_epoch(pl.col("timestamp"), time_unit="ms").alias(
+                        "timestamp"
+                    ),
+                    pl.col("close").alias(f"{ticker}_close"),
+                    pl.col("close")
+                    .pct_change()
+                    .add(1)
+                    .fill_null(1)
+                    .alias(f"{ticker}_return"),
+                ]
+            )
         )
-        ticker_return_expressions.append(
-            pl.col(f"{ticker}_close").pct_change().alias(f"{ticker}_return")
+        if combined_history is None:
+            combined_history = df
+        else:
+            combined_history = combined_history.join(df, on="timestamp", how="right")
+
+    weights_lazy = (
+        weights.lazy()
+        .sort("timestamp")
+        .select(
+            pl.col("timestamp").alias("rebalance_date"),
+            *[pl.col(ticker) for ticker in tickers],
+        )
+    )
+    # Join weights with rebalance dates
+    backtest = combined_history.join_asof(
+        weights_lazy,
+        left_on="timestamp",
+        right_on="rebalance_date",
+        strategy="backward",
+    )
+
+    # cumulative return within each rebalance period
+    for ticker in tickers:
+        backtest = backtest.with_columns(
+            pl.col(f"{ticker}_return")
+            .cum_prod()
+            .over("rebalance_date")
+            .alias(f"{ticker}_period_return")
         )
 
-    backtest = (
-        backtest.with_columns(
+    backtest = backtest.with_columns(
+        pl.sum_horizontal(
             [
-                pl.from_epoch(pl.col("timestamp"), time_unit="ms").alias("datetime"),
-                *ticker_return_expressions,
-            ]
-        )
-        .with_columns(
-            pl.sum_horizontal(
-                [pl.col(f"{ticker}_return") * pl.col(ticker) for ticker in tickers]
-            ).alias("daily_return")
-        )
-        .with_columns(
-            pl.col("daily_return").add(1).cum_prod().sub(1).alias("cumulative_return"),
-        )
-        .with_columns(
-            [
-                pl.col(f"{ticker}_return")
-                .add(1)
-                .cum_prod()
-                .sub(1)
-                .alias(f"{ticker}_cumulative_return")
+                pl.col(f"{ticker}_period_return")
+                .mul(pl.col(ticker))
+                .alias(f"{ticker}_weighted_return")
                 for ticker in tickers
             ]
+        ).alias("period_total_weighted")
+    )
+
+    # previous period's last value
+    aggregated_period = (
+        backtest.group_by("rebalance_date")
+        .agg(pl.col("period_total_weighted").last())
+        .sort("rebalance_date")
+        .with_columns(
+            pl.col("period_total_weighted")
+            .shift()
+            .fill_null(1)
+            .cum_prod()
+            .alias("previous_period_last")
         )
+    )
+    backtest = backtest.join(aggregated_period, on="rebalance_date", how="right")
+
+    # cumulative returns
+    backtest = backtest.with_columns(
+        [
+            pl.col(f"{ticker}_return").cum_prod().alias(f"{ticker}_cumulative_return")
+            for ticker in tickers
+        ]
+    ).with_columns(
+        pl.col("period_total_weighted")
+        .mul(pl.col("previous_period_last"))
+        .alias("overall_cumulative_return"),
     )
 
     return backtest.collect()
 
 
-def plot_backtest(backtest):
+def plot_backtest(backtest, tickers=[]):
     """
     Plots the cumulative return of the backtest.
     """
-    plt.plot(backtest["datetime"], backtest["cumulative_return"])
+    plt.plot(
+        backtest["timestamp"], backtest["overall_cumulative_return"], label="Portfolio"
+    )
+    for ticker in tickers:
+        plt.plot(
+            backtest["timestamp"],
+            backtest[f"{ticker}_cumulative_return"],
+            label=ticker,
+        )
+    plt.legend()
     plt.title("Backtest Returns")
     plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y\n%b"))
@@ -111,18 +168,23 @@ if __name__ == "__main__":
         raise ValueError("POLYGON_API_KEY is not set in environment variables")
 
     client = RESTClient(api_key)
-
-    tickers = ["AAPL", "NVDA"]
-    history = load_data(tickers)
-    aapl = history["AAPL"]
+    stock1 = "NVDA"
+    stock2 = "UVXY"
+    tickers = [stock1, stock2]
+    history = load_data(
+        tickers, timespan="day", from_time="2024-01-01", to_time="2024-12-06"
+    )
     # make weights dataframe with 0.5 for each stock and matching timestamps of history
     weights = pl.DataFrame(
         {
-            "AAPL": [0.5] * len(aapl),
-            "NVDA": [0.5] * len(aapl),
-            "timestamp": aapl["timestamp"],
+            stock1: [0.9, 0],
+            stock2: [0.1, 1],
+            "timestamp": pl.Series(
+                ["01/01/2024 17:00:00.000", "06/06/2024 17:00:00.000"]
+            ).str.strptime(pl.Datetime, "%d/%m/%Y %H:%M:%S%.3f"),
         }
     )
-    backtest_result = backtest(history, weights)
-    print(f"Total return: {backtest_result['cumulative_return'].last():.003%}")
+    backtest_result = backtest(history, weights, tickers)
+
+    print(f"Total return: {backtest_result['overall_cumulative_return'].last():.02%}")
     plot_backtest(backtest_result)
